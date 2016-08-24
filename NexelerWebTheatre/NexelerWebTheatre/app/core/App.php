69e2711f529d13289be835edf755963d61517ecf
JelenaNexeler <jelena.derikonjic@outlook.com>
2016-08-24 12:04:40 +0200

    Update __constuct() method;
    Complete parsing url

--- NexelerWebTheatre/NexelerWebTheatre/app/core/App.php
+++ NexelerWebTheatre/NexelerWebTheatre/app/core/App.php
@@ -17,6 +17,30 @@ class App
     public function __construct()
     {
         $url = $this->parseUrl();
+        
+        if(file_exists('../app/controlles/' . $url[0] . '.php'))
+        {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        
+        require_once '../app/controlles/' . $this->controller . '.php';
+        
+        $this->controller = new $this->controller;
+         
+        if(isset($url[1]))
+        {
+            if(method_exists($this->controller, $url[1]))
+            {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+        
+        $this->params = $url ? array_values($url) : [];
+        
+        call_user_func_array([$this->controller, $this->method], $this->params);
+         
     }
 
     protected function parseUrl()
@@ -25,8 +49,6 @@ class App
         {
             return $url = explode('/',filter(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
         }
-
-
     }
 }
 ?>
