3805ff409c0a790721dfe274a7811c8388f36895
JelenaSt <jecastan91@gmail.com>
2016-08-27 22:07:27 +0200

    Footer textualna ispravka

--- NexelerWebTheatre/NexelerWebTheatre/app/views/templates/footer.php
+++ NexelerWebTheatre/NexelerWebTheatre/app/views/templates/footer.php
@@ -1,3 +1,3 @@
-	<div style="position:fixed;left:0px;right:0px;bottom:0px;text-align:center;padding-bottom:10px;"><hr/>Nekretnine DOO - Copyright 2016</div>
+	<div style="position:fixed;left:0px;right:0px;bottom:0px;text-align:center;padding-bottom:10px;"><hr/>WEBPOZORI&#352;TE NEXELER - Copyright 2016</div>
 </body>
 </html>
