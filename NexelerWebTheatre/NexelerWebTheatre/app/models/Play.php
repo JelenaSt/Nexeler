09ca8d814abac25638180e85014d1c13d68f1df0
JelenaSt <jecastan91@gmail.com>
2016-08-30 00:03:42 +0200

    Play i performances beta

--- NexelerWebTheatre/NexelerWebTheatre/app/models/Play.php
+++ NexelerWebTheatre/NexelerWebTheatre/app/models/Play.php
@@ -3,33 +3,109 @@
 class Play
 {
   var $playId;
-  var &play_name;
-  var &short_desc;
-  var &description;
+  var $playTitle;
+  var $description;
   
-  public function __constuct($playId,&play_name,&short_desc,&description)
-  {
+  public function __constuct($args)
+    {
+        $this->playId = $args["playId"];
+        $this->playTitle = $args["playTitle"];
+        $this->description = $args["description"];
+    }
   
-  }
+  public static function writeNewPlayInDatabase($playTitle,$description)
+    {
+        $database = Database::getInstance()->getConnection();
+
+        $sql = "SELECT MAX ID FROM plays";
+        $query_result = mysqli_query($database, $sql);
+
+        $row = mysqli_fetch_array($query_result);
+        $playId = $row + 1;
+
+        $sql = "INSERT INTO plays (ID,Title,Description)
+                    VALUES ('$playId','$playTitle','$description')";
+        $query_result = mysqli_query($database, $sql);
+        print_r($query_result);
+        if ($query_result === TRUE) {
+            return true;
+        }
+        return false;
+    }
   
-  public static function writeNewPlayInDatabase(&play_name,&short_desc,&description)
-  {
+    public static function getPlayByID($play_id)
+    {
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM plays where ID ='$play_id'";
+
+        $result = mysqli_query($dbConnection,$sql);
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            
+            $play= new Play($row);
+            return $play;
+        }
+
+        return NULL;
+    }
   
-  }
-  
-  public static funstion getPlayByID($play_id)
-  {
-  }
-  
-  public static funstion getPlayByName($play_name)  
-  {
-  }
-  
-  public static funstion fetchAllPlays()  
-  {
-  }
-  
-  public static funstion deletePlayByID($play_id)
-  {
-  }
+    public static function getPlayByName($play_name)  
+    {
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM plays where Title ='$play_name'";
+
+        $result = mysqli_query($dbConnection,$sql);
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            
+            $play= new Play($row);
+            return $play;
+        }
+
+        return NULL;
+    
+    }
+    
+    public static function fetchAllPlays()  
+    {
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM plays";
+        mysqli_query($dbConnection, "set names 'utf8'");
+        $result = mysqli_query($dbConnection,$sql);
+        if ($result->num_rows > 0)
+        {
+            return $result;
+        }
+        return NULL;
+    }
+    
+    public static function deletePlayByID($play_id)
+    {
+        $database = Database::getInstance()->getConnection();
+    	
+    	$sql = "DELETE FROM play WHERE ID='$play_id'";
+    	
+        $query_result = $database->query($sql);
+        if ($query_result === TRUE) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function getPlayPictureById($play_id)
+    {
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM playpictures where ID ='$play_id'";
+
+        $result = mysqli_query($dbConnection,$sql);
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            return $row['PlayPicture'];
+        }
+        return NULL;
+    }
+
 }
--- NexelerWebTheatre/NexelerWebTheatre/app/views/pages/preformances.php
+++ NexelerWebTheatre/NexelerWebTheatre/app/views/pages/preformances.php
@@ -1,5 +1,55 @@
+<div class="page-body">
+
+<h1>Predstave</h1>
+
 <?php
 
-echo 'Hello from Preformaces page';
+require_once(dirname(__FILE__)."\..\..\models\Play.php");
+header("Content-Type: text/html;charset=utf-8");
+$plays = Play::fetchAllPlays();
 
+if ($plays->num_rows > 0)
+{
+    foreach($plays as $play)
+    {
+        $id = $play['ID'];
+        $title = $play['Title'];
+		$description = $play['Description'];
+        
+        $picture = Play::getPlayPictureById($id);
 ?>
+
+        <table style="width: 80%">
+            <col width="30%">
+            <col width="70%">
+		<tr>
+		<th><label><?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $picture ).'" width="200"/>'.'<br>'; ?></label></th>
+		<th><label>
+		
+		<h2><?php echo "$title "; ?></h2>
+		<?php  
+        $descShort = substr("$description",0,400);
+        echo "$descShort"."...".'<br>'.'<br>' ;	
+        ?>
+		
+		<button class="button">Detaljnije</button>
+		
+		<?php
+        if (Session::get('user_level') == 2)
+        {
+        ?>
+			<button class="button">Izmeni</button>
+		<?php 
+        } 
+        ?>
+		
+		</label></th>
+        </tr>
+        </table>
+
+ <?php 
+	}
+}
+
+ ?>
+</div>
