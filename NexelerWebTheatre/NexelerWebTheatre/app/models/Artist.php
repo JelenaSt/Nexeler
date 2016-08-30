a6e800055ca99dc71f4614d5dd78b5d4da2e626a
JelenaSt <jecastan91@gmail.com>
2016-08-30 10:20:05 +0200

    Update Artist.php

--- NexelerWebTheatre/NexelerWebTheatre/app/models/Artist.php
+++ NexelerWebTheatre/NexelerWebTheatre/app/models/Artist.php
@@ -7,110 +7,173 @@ class Artist
   var $artistBiography;
   
   //kako se stvarno zovu kolone tamo, bem li ga
-  public function __construct($args)
-  {
-    $this->artistId = $args["ID"];
-    $this->artistName = $args["Name"];
-    $this->artistBiography = $args["Biography"];
-  }
-  
-  public static function writeNewActorInDatabase($actorName,$actorBio)
-  {
-    $database = Database::getInstance()->getConnection();
-    $sql = "SELECT MAX ID 
-            FROM artists";
-    $query_result = mysqli_query($database, $sql);
-    $row = mysqli_fetch_array($query_result);
-    
-    $artistId = $row + 1;
-    $sql = "INSERT INTO artists (ID,Name,Biography)
-            VALUES ('$artistId','$actorName','$actorBio')";
-    $query_result = mysqli_query($database, $sql);
-    print_r($query_result);
-    if ($query_result === TRUE) 
+    public function __construct($args)
     {
-      return true;
+        $this->artistId = $args["ID"];
+        $this->artistName = $args["Name"];
+        $this->artistBiography = $args["Biography"];
     }
-    return false;
-  }
   
-  public static function updateActor($actor_id,$actor_name,$actor_desc)
-  {
-    $database = Database::getInstance()->getConnection();
-    
-    $sql = "UPDATE artists SET Name='$actor_name', Biography='$actor_desc'
-            WHERE ID='$actor_id'";
-            
-    $query_result = $database->query($sql);
-    
-    if ($query_result === TRUE) 
+    public static function writeNewArtistInDatabase($artistName,$artistBio)
     {
-       return true;
+        $database = Database::getInstance()->getConnection();
+        $sql = "SELECT MAX ID 
+                FROM artists";
+        $query_result = mysqli_query($database, $sql);
+        $row = mysqli_fetch_array($query_result);
+        
+        $artistId = $row + 1;
+        $sql = "INSERT INTO artists (ID,Name,Biography)
+                VALUES ('$artistId','$artistName','$artistBio')";
+        $query_result = mysqli_query($database, $sql);
+        print_r($query_result);
+        if ($query_result === TRUE) 
+        {
+          return true;
+        }
+        return false;
     }
-    return false;
-  }
   
-  public static function fetchAllActors()
-  {
-    $dbConnection = Database::getInstance()->getConnection();
-    $sql = "SELECT * FROM artists";
-    mysqli_query($dbConnection, "set names 'utf8'");
-    $result = mysqli_query($dbConnection,$sql);
-    
-    if ($result->num_rows > 0)
+    public static function updateArtist($artistId,$artistName,$artistBio)
     {
-      return $result;
+        $database = Database::getInstance()->getConnection();
+        
+        $sql = "UPDATE artists SET Name='$artistName', Biography='$artistBio'
+                WHERE ID='$artistId'";
+                
+        $query_result = $database->query($sql);
+        
+        if ($query_result === TRUE) 
+        {
+             return true;
+        }
+        
+        return false;
     }
-     return NULL;
-  }
+    
+    public static function fetchAllArtist()
+    {
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM artists";
+        mysqli_query($dbConnection, "set names 'utf8'");
+        $result = mysqli_query($dbConnection,$sql);
+        
+        if ($result->num_rows > 0)
+        {
+           return $result;
+        }
+         return NULL;
+    }
   
-  public static function getActorByID($actor_id)
-  {
-    $dbConnection = Database::getInstance()->getConnection();
-    $sql = "SELECT * FROM artists 
-            WHERE ID ='$actor_id'";
-            
-    $result = mysqli_query($dbConnection,$sql);
-    
-    if(mysqli_num_rows($result) == 1)
+    public static function getArtistByID($artistId)
     {
-      $row = mysqli_fetch_assoc($result);
-      $artist = new Artist($row);
-      return $artist;
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM artists 
+                WHERE ID ='$artistId'";
+                
+        $result = mysqli_query($dbConnection,$sql);
+        
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $artist = new Artist($row);
+            return $artist;
+        }
+        return NULL;
     }
-    return NULL;
-  }
-  
-  public static function getActorByName($actor_name)
-  {
-    $dbConnection = Database::getInstance()->getConnection();
-    $sql = "SELECT * FROM artists 
-            WHERE Name ='$actor_name'";
-            
-    $result = mysqli_query($dbConnection,$sql);
     
-    if(mysqli_num_rows($result) == 1)
+    public static function getArtistByName($artistName)
     {
-      $row = mysqli_fetch_assoc($result);
-      $artist = new Artist($row);
-      return $artist;
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM artists 
+                WHERE Name ='$artistName'";
+                
+        $result = mysqli_query($dbConnection,$sql);
+        
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $artist = new Artist($row);
+            return $artist;
+        }
+        return NULL;
     }
-    return NULL;
-  }
-  
-  public static function deleteActorByID($actor_id)
-  {
-    $database = Database::getInstance()->getConnection();
-    $sql = "DELETE FROM artists 
-            WHERE ID='$actor_id'";
-    	
-    $query_result = $database->query($sql);
     
-    if ($query_result === TRUE) 
+    public static function deleteArtistByID($artistId)
     {
-      return true;
+        $database = Database::getInstance()->getConnection();
+        $sql = "DELETE FROM artists 
+                WHERE ID='$artistId'";
+        	
+        $query_result = $database->query($sql);
+        
+        if ($query_result === TRUE) 
+        {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
+    
+    //picture related methods
+    public static function writeNewArtistPictureInDatabase($artistId,$artistPicture)
+    {
+        $database = Database::getInstance()->getConnection();
+        $sql = "INSERT INTO artistpictures (ArtistID,ArtistPicture)
+                VALUES ('$artistId','$artistPicture')";
+        $query_result = mysqli_query($database, $sql);
+        print_r($query_result);
+        if ($query_result === TRUE) 
+        {
+          return true;
+        }
+        return false;
+    }
+    
+    public static function getArtistPictureById($artistId)
+    {
+        $dbConnection = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM artistpictures 
+                 WHERE ArtistID ='$artistId'";
+        $result = mysqli_query($dbConnection,$sql);
+       
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            return $row['ArtistPicture'];
+        }
+        return NULL;
+    }
+
+    public static function deleteActorPictureByID($artistId)
+    {
+        $database = Database::getInstance()->getConnection();
+        $sql = "DELETE FROM artistpictures 
+                WHERE ArtistID='$artistId'";
+        	
+        $query_result = $database->query($sql);
+        
+        if ($query_result === TRUE) 
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    public static function updateArtistPicture($artistId,$picture)
+    {
+        $database = Database::getInstance()->getConnection();
+        
+        $sql = "UPDATE artists SET ArtistPicture='$picture'
+                WHERE ArtistID='$artistId'";
+                
+        $query_result = $database->query($sql);
+        
+        if ($query_result === TRUE) 
+        {
+             return true;
+        }
+        
+        return false;
+    }
   
 }
