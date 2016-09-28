239ae2a78ba5b02cdff583ec5893ed7e34a98810
JelenaSt <jecastan91@gmail.com>
2016-09-28 10:36:40 +0200

    Update register.php

--- NexelerWebTheatre/NexelerWebTheatre/app/views/usermanagement/register.php
+++ NexelerWebTheatre/NexelerWebTheatre/app/views/usermanagement/register.php
@@ -26,7 +26,7 @@
                     <td><input type="text" id="last_name" name="last_name" placeholder="Prezime"/></td>
                 </tr>
                 <tr>
-                    <td><label>Korisni?ko ime</label></td>
+                    <td><label>Korisniƒçko ime</label></td>
                     <td>
                        <!-- <div class="container"></div>-->
                             <input type="text" id="user_name" name="user_name" placeholder="Korisni?ko ime" onBlur="checkAvailability()" style="width:70%"/>
@@ -39,19 +39,19 @@
                     <td><input type="text" id="email" name="email" placeholder="example@example.com" /></td>
                 </tr>
                 <tr>
-                    <td><label>äifra</label></td>
-                    <td><input type="password" id="password" name="password" placeholder="äifra" /></td>
+                    <td><label>≈†ifra</label></td>
+                    <td><input type="password" id="password" name="password" placeholder="≈†ifra" /></td>
                 </tr>
                  <tr>
-                    <td><label>Ponovite öifru</label></td>
-                    <td><input type="password" id="password_repeat" name="password_repeat" placeholder="äifra" /></td>
+                    <td><label>Ponovite ≈°ifru</label></td>
+                    <td><input type="password" id="password_repeat" name="password_repeat" placeholder="≈†ifra" /></td>
                 </tr>
                 <!--<tr><td></td> <td> <label id="user-availability-status"></label></td></tr>-->
             </table>
              
             <label id="validation-status"></label>
-            <!--<input type="button" name="login_button" id="login_button" value="Poöalji zahtev">-->
-           <button id="login_button">Poöalji zahtev</button>
+            <!--<input type="button" name="login_button" id="login_button" value="Po≈°alji zahtev">-->
+           <button id="login_button">Po≈°alji zahtev</button>
         </form>
 
        
